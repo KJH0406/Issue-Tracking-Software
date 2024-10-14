e1b2249689d580a06848e7ffaeb2cf7794ad5a9e
김장호 <rlawkdgh0406@naver.com>
2024-10-14 20:52:08 +0900

    feat: 인증 페이지 기본 레이아웃 구현(로직 미포함) - 로고 및 회원 가입 버튼 추가

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,12 +1,24 @@
+import { Button } from "@/components/ui/button"
+import Image from "next/image"
+
 interface AuthLayoutProps {
   children: React.ReactNode
 }
 
+// 인증 페이지 레이아웃(로그인, 회원가입)
 const AuthLayout = ({ children }: AuthLayoutProps) => {
   return (
-    <div>
-      <div>{children}</div>
-    </div>
+    <main className="bg-neutral-100 min-h-screen">
+      <div className="mx-auto max-w-screen-2xl p-4">
+        <nav className="flex justify-between items-center">
+          <Image src="/logo.svg" alt="logo" width={152} height={56} />
+          <Button variant="secondary">Sign up</Button>
+        </nav>
+        <div className="flex flex-col items-center justify-center pt-4 md:pt-14">
+          {children}
+        </div>
+      </div>
+    </main>
   )
 }
 
