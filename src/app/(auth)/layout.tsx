b5898bbf6451cfe93ae38ea26dcf273e9f5fa231
김장호 <rlawkdgh0406@naver.com>
2024-10-14 23:49:04 +0900

    feat: 회원가입,로그인 버튼 경로에 따른 동적 렌더링 추가

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,5 +1,10 @@
-import { Button } from "@/components/ui/button"
+"use client"
+
 import Image from "next/image"
+import { usePathname } from "next/navigation"
+
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
 
 interface AuthLayoutProps {
   children: React.ReactNode
@@ -7,12 +12,22 @@ interface AuthLayoutProps {
 
 // 인증 페이지 레이아웃(로그인, 회원가입)
 const AuthLayout = ({ children }: AuthLayoutProps) => {
+  // 현재 페이지 경로
+  const pathname = usePathname()
+
+  // 페이지 전환 플레그(로그인 / 회원가입)
+  const isSignIn = pathname === "/sign-in"
+
   return (
     <main className="bg-neutral-100 min-h-screen">
       <div className="mx-auto max-w-screen-2xl p-4">
         <nav className="flex justify-between items-center">
           <Image src="/logo.svg" alt="logo" width={152} height={56} />
-          <Button variant="secondary">Sign up</Button>
+          <Button asChild variant="secondary">
+            <Link href={isSignIn ? "/sign-up" : "sign-in"}>
+              {isSignIn ? "회원가입" : "로그인"}
+            </Link>
+          </Button>
         </nav>
         <div className="flex flex-col items-center justify-center pt-4 md:pt-14">
           {children}
