2bdcca8a63086a46de60621e0d717e0616ef0d09
김장호 <rlawkdgh0406@naver.com>
2024-10-16 22:02:19 +0900

    feat: Appwrite와 세션 쿠키를 사용한 회원가입 기능 구현 - Appwrite 클라이언트를 설정하는  함수 추가 - Hono를 사용하여 회원가입 라우트 구현 - 세션 쿠키 설정 (secure, httpOnly, sameSite 정책 적용)

--- src/app/features/auth/server/route.ts
+++ src/app/features/auth/server/route.ts
@@ -1,6 +1,11 @@
 import { Hono } from "hono"
+import { ID } from "node-appwrite"
 import { zValidator } from "@hono/zod-validator"
+import { setCookie } from "hono/cookie"
 
+import { createAdminClient } from "@/lib/appwrite"
+
+import { AUTH_COOKIE } from "../constants"
 import { loginSchema, registerSchema } from "../schema"
 
 const app = new Hono()
@@ -26,9 +31,25 @@ const app = new Hono()
     async (c) => {
       const { name, email, password } = c.req.valid("json")
 
-      console.log({ name, email, password })
+      // Appwrite 클라이언트의 Account 객체 접근
+      const { account } = await createAdminClient()
 
-      return c.json({ name, email, password })
+      // 새로운 사용자를 Appwrite에 등록
+      const user = await account.create(ID.unique(), email, password, name)
+
+      // 로그인 세션 정보 반환
+      const session = await account.createEmailPasswordSession(email, password)
+
+      // 로그인 후 세션 쿠키를 설정(인증된 상태를 유지)
+      setCookie(c, AUTH_COOKIE, session.secret, {
+        path: "/", // 모든 경로에서 유효
+        httpOnly: true, // XSS 방지
+        secure: true,
+        sameSite: "strict", // CSRF 방지
+        maxAge: 60 * 60 * 24 * 30, // 유효 기간 설정(30일)
+      })
+
+      return c.json({ data: user })
     }
   )
 
