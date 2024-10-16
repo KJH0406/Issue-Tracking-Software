64918c799f6990207749aa1fb0ccd6d37697ec98
김장호 <rlawkdgh0406@naver.com>
2024-10-16 23:39:22 +0900

    feat: 로그아웃 기능 추가 및 인증 상태 자동 업데이트(리디렉션) -  훅 추가: 서버에 로그아웃 요청을 보내고 React Query로 캐시된 사용자 데이터를 무효화하여 로그인 페이지로 리디렉션 -  페이지에 로그아웃 버튼 추가: 로그아웃 시 로그인 페이지로 리디렉션 처리

---
+++ src/app/features/auth/api/use-logout.ts
@@ -0,0 +1,27 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { InferResponseType } from "hono"
+
+import { client } from "@/lib/rpc"
+
+type ResponseType = InferResponseType<(typeof client.api.auth.logout)["$post"]>
+
+// 로그아웃 처리
+export const useLogout = () => {
+  const queryClient = useQueryClient()
+
+  const mutation = useMutation<ResponseType, Error>({
+    mutationFn: async () => {
+      // 서버에 로그아웃 요청
+      const response = await client.api.auth.logout["$post"]()
+      return await response.json()
+    },
+
+    // 로그아웃 성공 시 캐시된 현재 사용자 데이터 무효화(사용자 데이터 새로고침)
+    // 로그인 페이지로 리디렉션
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["current"] })
+    },
+  })
+
+  return mutation
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,17 @@ import { useEffect } from "react"
 import { useRouter } from "next/navigation"
 
 import { useCurrent } from "@/app/features/auth/api/use-current"
+import { useLogout } from "@/app/features/auth/api/use-logout"
+
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   const router = useRouter()
   // 현재 사용자 데이터
   const { data, isLoading } = useCurrent()
+
+  // 로그아웃 Mutation 함수
+  const { mutate } = useLogout()
 
   useEffect(() => {
     // 사용자가 인증되지 않았고, 로딩이 끝난 경우 로그인 페이지로 리디렉션
@@ -16,5 +22,16 @@ export default function Home() {
       router.push("/sign-in")
     }
   }, [data])
-  return <div>인증된 사용자만 접근 가능한 페이지입니다.</div>
+  return (
+    <div>
+      인증된 사용자만 접근 가능한 페이지입니다.
+      <Button
+        onClick={() => {
+          mutate()
+        }}
+      >
+        로그아웃
+      </Button>
+    </div>
+  )
 }
