20c1ee6c74625e6c6c6becbb6630210657d0059f
김장호 <rlawkdgh0406@naver.com>
2024-10-19 23:47:58 +0900

    feat: 멤버 테이블 생성(DB) 및 워크스페이스 생성 로직 수정 - Appwrite DB에 'members' 컬렉션 생성     - userId, workspaceId, role (enum: ADMIN, MEMBER) - 워크스페이스 생성 프로세스 수정     - 워크스페이스 생성 시 자동으로 관리자 멤버 생성     - 워크스페이스에 inviteCode 속성 추가

--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -1,30 +1,57 @@
-import { Hono } from "hono";
-import { ID, Query } from "node-appwrite";
-import { zValidator } from "@hono/zod-validator";
+import { Hono } from "hono"
+import { ID, Query } from "node-appwrite"
+import { zValidator } from "@hono/zod-validator"
 
-import { sessionMiddleware } from "@/lib/session-middleware";
-import { DATABASE_ID, WORKSPACE_ID, IMAGES_BUCKET_ID } from "@/config";
+import { sessionMiddleware } from "@/lib/session-middleware"
+import { generateInviteCode } from "@/lib/utils"
 
-import { createWorkspaceSchema } from "../schemas";
+import {
+  DATABASE_ID,
+  WORKSPACE_ID,
+  IMAGES_BUCKET_ID,
+  MEMBERS_ID,
+} from "@/config"
 
+import { createWorkspaceSchema } from "../schemas"
+import { MemberRole } from "@/features/members/types"
 
 const app = new Hono()
 
-// 워크스페이스 목록 가져오기
-.get("/", sessionMiddleware, async (c) => {
-  const databases = c.get("databases")
+  // 워크스페이스 목록 가져오기
+  .get("/", sessionMiddleware, async (c) => {
+    const user = c.get("user") // 전체 사용자 가져오기
+    const databases = c.get("databases") // 데이터베이스 가져오기
 
-  // 워크스페이스 가져오기
-  const workspaces = await databases.listDocuments(
-    DATABASE_ID,
-    WORKSPACE_ID
-  )
+    // 워크스페이스 멤버 가져오기
+    const members = await databases.listDocuments(DATABASE_ID, MEMBERS_ID, [
+      // 사용자 ID가 일치하는 멤버 가져오기
+      Query.equal("userId", user.$id),
+    ])
 
-  return c.json({data: workspaces})
-})
+    // 멤버가 없으면 워크스페이스 생성 페이지로 이동
+    if (members.total === 0) {
+      return c.json({ data: { documents: [], total: 0 } })
+    }
 
-// 워크스페이스 생성 하기
-.post(
+    // 워크스페이스 ID 가져오기
+    const workspaceIds = members.documents.map((member) => member.workspaceId)
+
+    // 워크스페이스 가져오기
+    const workspaces = await databases.listDocuments(
+      DATABASE_ID,
+      WORKSPACE_ID,
+      [
+        // 워크스페이스 ID가 일치하는 워크스페이스 가져오기
+        Query.orderDesc("$createdAt"),
+        Query.contains("$id", workspaceIds),
+      ]
+    )
+
+    return c.json({ data: workspaces })
+  })
+
+  // 워크스페이스 생성 하기
+  .post(
     "/",
     zValidator("form", createWorkspaceSchema),
     // 세션 가져오기
@@ -34,16 +61,20 @@ const app = new Hono()
       const databases = c.get("databases") // 데이터베이스 가져오기
       const storage = c.get("storage") // 스토리지 가져오기
       const user = c.get("user") // 사용자 가져오기
-      
+
       const { name, image } = c.req.valid("form") // 폼 데이터 가져오기
 
       // 업로드된 이미지의 URL을 저장할 변수 초기화
-      let uploadedImageUrl: string | undefined 
+      let uploadedImageUrl: string | undefined
 
       // 이미지가 제공되었는지 확인
       if (image instanceof File) {
         // 스토리지에 이미지 파일 업로드
-        const file = await storage.createFile(IMAGES_BUCKET_ID, ID.unique(), image)
+        const file = await storage.createFile(
+          IMAGES_BUCKET_ID,
+          ID.unique(),
+          image
+        )
 
         // 업로드된 이미지의 미리보기 가져오기
         const arrayBuffer = await storage.getFilePreview(
@@ -52,18 +83,33 @@ const app = new Hono()
         )
 
         // ArrayBuffer를 Base64 문자열로 변환하여 데이터 URL 생성
-        uploadedImageUrl = `data:image/png;base64,${Buffer.from(arrayBuffer).toString("base64")}`
+        uploadedImageUrl = `data:image/png;base64,${Buffer.from(
+          arrayBuffer
+        ).toString("base64")}`
       }
 
       // 데이터베이스에 새 워크스페이스 문서 생성
-      const workspace = await databases.createDocument(DATABASE_ID, WORKSPACE_ID, ID.unique(), {
-        name,                // 워크스페이스 이름
-        userId: user.$id,    // 사용자 ID
-        imageUrl: uploadedImageUrl  // 업로드된 이미지 URL (이미지가 없으면 undefined)
+      const workspace = await databases.createDocument(
+        DATABASE_ID,
+        WORKSPACE_ID,
+        ID.unique(),
+        {
+          name, // 워크스페이스 이름
+          userId: user.$id, // 사용자 ID
+          imageUrl: uploadedImageUrl, // 업로드된 이미지 URL (이미지가 없으면 undefined)
+          inviteCode: generateInviteCode(6), // 초대 코드 생성(최대 길이 10)
+        }
+      )
+
+      // 워크스페이스 생성 시 관리자 자동 생성
+      await databases.createDocument(DATABASE_ID, MEMBERS_ID, ID.unique(), {
+        userId: user.$id,
+        workspaceId: workspace.$id,
+        role: MemberRole.ADMIN,
       })
 
       // 워크스페이스 생성 성공
-      return c.json({data: workspace})
+      return c.json({ data: workspace })
     }
   )
 
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,20 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+// 클래스 이름 병합(스타일)
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+// 초대 코드 생성
+export function generateInviteCode(length: number) {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+  let result = ""
+
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length))
+  }
+
+  return result
 }
